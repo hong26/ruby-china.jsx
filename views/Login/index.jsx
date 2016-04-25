2f34fe9d4882edf74444507b729429596f82ea89
sk.music <sk.music@git.io>
2016-04-25 13:56:49 +0800

    018bf45f2211600c94213a18f7ca2161d16c0cbd

--- views/Login/index.jsx
+++ views/Login/index.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
 
 module.exports = React.createClass({
+  getInitialState() {
+    return {
+      login: {}
+    }
+  },
   render() {
-    return <div className="pages-404">
+    return <div className="container">
+      <form accept-charset="UTF-8">
+        <div className="form-group">
+          <label htmlFor="username"></label>
+          <input type="text" name="username" value={this.state.login.username} className="form-control" />
+        </div>
+      </form>
     </div>
   }
 })
