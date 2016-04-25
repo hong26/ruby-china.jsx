801358d1b23180915aa58017ae4e79f7885008f5
sk.music <sk.music@git.io>
2016-04-25 14:29:10 +0800

    018bf45f2211600c94213a18f7ca2161d16c0cbd

--- routes/index.jsx
+++ routes/index.jsx
@@ -8,10 +8,12 @@ import {
 
 import NotFound from '../views/404'
 import Application from '../views/app'
+import Login from '../views/Login'
 
 module.exports = () => {
   return <Router history={browserHistory}>
     <Route path="/" component={Application}>
+      <Route path="login" component={Login} />
       <Route path="*" component={NotFound} />
     </Route>
   </Router>
--- views/Login/index.jsx
+++ views/Login/index.jsx
@@ -1,18 +1,38 @@
 import React from 'react'
+import {locales} from '../../settings'
 
 module.exports = React.createClass({
   getInitialState() {
     return {
-      login: {}
+      login: {
+        username: '',
+        password: ''
+      }
     }
+  },
+  handleSubmit(event) {
+    event.preventDefault()
+  },
+  handleChange(event) {
+    const login = this.state.login
+    login[event.target.name] = event.target.value
+
+    this.setState({
+      login: login
+    })
   },
   render() {
     return <div className="container">
-      <form accept-charset="UTF-8">
+      <form accept-charset="UTF-8" onSubmit={this.handleSubmit}>
         <div className="form-group">
-          <label htmlFor="username"></label>
-          <input type="text" name="username" value={this.state.login.username} className="form-control" />
+          <label htmlFor="username">{locales.zh_CN.username}</label>
+          <input type="text" name="username" value={this.state.login.username} className="form-control" onChange={this.handleChange} />
         </div>
+        <div className="form-group">
+          <label htmlFor="password">{locales.zh_CN.password}</label>
+          <input type="password" name="password" value={this.state.login.password} className="form-control" onChange={this.handleChange} />
+        </div>
+        <button className="btn btn-primary">{locales.zh_CN.login}</button>
       </form>
     </div>
   }
