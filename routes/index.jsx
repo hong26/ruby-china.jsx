4976fb14b50cdf1ce58f7db2ae849e706c3695c6
sk.music <sk.music@git.io>
2016-04-26 14:11:24 +0800

    018bf45f2211600c94213a18f7ca2161d16c0cbd

--- routes/index.jsx
+++ routes/index.jsx
@@ -9,10 +9,12 @@ import {
 import NotFound from '../views/404'
 import Application from '../views/app'
 import Login from '../views/Login'
+import Home from '../views/Home'
 
 module.exports = () => {
   return <Router history={browserHistory}>
     <Route path="/" component={Application}>
+      <IndexRoute component={Home} />
       <Route path="login" component={Login} />
       <Route path="*" component={NotFound} />
     </Route>
