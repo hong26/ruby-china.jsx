1dc6740bee6e3a629f5bde22b8b90b4ab1d656a7
sk.music <sk.music@git.io>
2016-04-26 12:24:55 +0800

    018bf45f2211600c94213a18f7ca2161d16c0cbd

--- views/app.jsx
+++ views/app.jsx
@@ -10,23 +10,23 @@ module.exports = React.createClass({
     }
   },
   componentDidMount() {
+    this.setAuthorizedUser()
+  },
+  setAuthorizedUser() {
     this.setState({
       accessToken: localStorage.getItem('access_token')
     }, () => {
-      this.setAuthorizedUser()
-    })
-  },
-  setAuthorizedUser() {
-    if (this.state.accessToken == null) {
-      return false
-    }
-
-    fetch(RUBY_CHINA_API_V3_URL + '/users/me?access_token=' + this.state.accessToken).then((response) => response.json()).then((responseJSON) => {
-      if (responseJSON.user) {
-        this.setState({
-          authorizedUser: responseJSON.user
-        })
+      if (this.state.accessToken == null) {
+        return false
       }
+
+      fetch(RUBY_CHINA_API_V3_URL + '/users/me?access_token=' + this.state.accessToken).then((response) => response.json()).then((responseJSON) => {
+        if (responseJSON.user) {
+          this.setState({
+            authorizedUser: responseJSON.user
+          })
+        }
+      })
     })
   },
   render() {
