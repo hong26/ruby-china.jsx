a3d19d73dee889234ce703261df45936ca7f7d73
sk.music <sk.music@git.io>
2016-04-26 11:38:57 +0800

    018bf45f2211600c94213a18f7ca2161d16c0cbd

--- views/app.jsx
+++ views/app.jsx
@@ -13,21 +13,13 @@ module.exports = React.createClass({
   },
   setAuthorizedUser() {
     const accessToken = localStorage.getItem('access_token')
-    const authorizedUser = localStorage.getItem('authorized_user')
 
     if (accessToken == null) {
       return false
     }
 
-    if (authorizedUser) {
-      return this.setState({
-        authorizedUser: JSON.parse(authorizedUser)
-      })
-    }
-
     fetch(RUBY_CHINA_API_V3_URL + '/users/me?access_token=' + accessToken).then((response) => response.json()).then((responseJSON) => {
       if (responseJSON.user) {
-        localStorage.setItem('authorized_user', JSON.stringify(responseJSON.user))
         this.setState({
           authorizedUser: responseJSON.user
         })
